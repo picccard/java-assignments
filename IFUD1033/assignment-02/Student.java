bbdd32a1c375020cf7da660a2ae847617ce40664
picccard <eskil.uhlving@gmail.com>
2018-02-26 08:18:58 +0100

    wrote Student class

--- IFUD1033/assignment-02/Student.java
+++ IFUD1033/assignment-02/Student.java
@@ -1,5 +1,45 @@
+import java.text.MessageFormat;
+
 public class Student {
-    public Student() {
+    private final String name;
+    private int countAssignments;
+    
+    public Student(String name) {
+        this.name = name;
+        this.countAssignments = 0;
+    }
+
+    public Student(String name, int startCount) {
+        this.name = name;
+        this.countAssignments = startCount;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCountAssignments() {
+        return this.countAssignments;
+    }
+
+    public void acceptAssignments(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException("Assignment-count must be a positive integer.");
+        }
+
+        this.countAssignments += count;
+    }
+
+    public String toString() {
+        String out = MessageFormat.format("Student: {0}, Assignments: {1}", this.name, this.countAssignments);
+        return out;
+    }
+
+    public static void main(String[] args) {
+        Student eskil = new Student("eskil");
+        System.out.println(eskil.toString());
+        eskil.acceptAssignments(1);
+        System.out.println(eskil.toString());
         
     }
 }
